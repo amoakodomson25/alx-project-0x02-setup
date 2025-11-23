a86172010846163981922a62cddad0c28bdee449
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-23 14:17:14 +0000

    q5

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,12 @@
+import { ButtonProps } from '@/interfaces'
+import React from 'react'
+
+const Button = ({size,shape}: ButtonProps) => {
+  return (
+    <div className={`${size} ${shape} border px-4 py-2 cursor-pointer`}>
+        Press meeeee!
+    </div>
+  )
+}
+
+export default Button
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,15 +1,21 @@
-import Header from '@/components/layout/Header'
-import React from 'react'
+import Button from "@/components/common/Button";
+import Header from "@/components/layout/Header";
+import React from "react";
 
 const about = () => {
-    return (
-        <>
-        <Header/>
-            <div className='text-center'>
-                <h1>About Page</h1>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="text-center">
+        <h1>About Page</h1>
+      </div>
+      <div className="flex gap-4 p-8 justify-center items-center">
+        <Button size="large" shape="rounded-full" />
+        <Button size="medium" shape="rounded-md" />
+        <Button size="small" shape="rounded-sm" />
+      </div>
+    </>
+  );
+};
 
-export default about
+export default about;
