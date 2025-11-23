bc2a4d38de67718ff38ad94db06bcd8bd269310a
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-23 13:39:05 +0000

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return <div className="flex flex-row justify-between px-8">
     Header
     <div className="flex gap-6">
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
+        <Link href="/home">Home</Link>
+        <Link href="/about">About</Link>
     </div>
     
     </div>;
