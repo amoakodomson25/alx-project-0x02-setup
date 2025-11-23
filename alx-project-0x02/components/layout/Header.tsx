5fe24aaa1044055e257160cb229a690ad6502614
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-23 14:19:41 +0000

    Q6

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div className="flex flex-row justify-between px-8 py-4">
-    Header
-    <div className="flex gap-6">
+  return (
+    <div className="flex flex-row justify-between px-8 py-4">
+      Header
+      <div className="flex gap-4">
         <Link href="/home">Home</Link>
-        <Link href="/about">About</Link>
+        <Link href="/about">About</Link> 
+        <Link href="/posts">Posts</Link>
+      </div>
     </div>
-    
-    </div>;
+  );
 };
 
 export default Header;
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/layout/Header'
+import React from 'react'
+
+const posts = () => {
+  return (
+    <>
+    <Header/>
+    <div className="text-center p-8">Posts</div>
+    </>
+  )
+}
+
+export default posts
